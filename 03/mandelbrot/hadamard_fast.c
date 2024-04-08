444248ddddda381e16f73c4e9bf48ec2b93a71f1
leoposc <schmid.leopold@web.de>
2024-04-08 12:07:08 +0200

    add exercise 01

---
+++ 03/mandelbrot/hadamard_fast.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	for(size_t i = 0; i < n; ++i) {
+		for(size_t j = 0; j < n; ++j) {
+			c[i][j] = a[i][j] * b[i][j];
+		}
+	}
+}
--- 03/mandelbrot/mandelbrot.c
+++ 03/mandelbrot/mandelbrot.c
@@ -13,7 +13,27 @@
 #define MAX_ITER 10000
 
 void calc_mandelbrot(uint8_t image[Y][X]) {
-	// TODO: Write your code here!
+	const float left = -2.5, right = 1;
+	const float bottom = -1.0, top = 1;
+
+	double x, y, cx, cy;
+	for(unsigned int i = 0; i < Y; i++) {
+		for(unsigned int j = 0; j < X; j++) {
+			x = 0.0;
+			y = 0.0;
+			unsigned int iter = 0;
+			cx = ((double)j / X) * (right - left) + left;
+			cy = ((double)i / Y) * (top - bottom) + bottom;
+			while(x * x + y * y <= 4.0 && iter < MAX_ITER) {
+				double x_tmp = x * x - y * y + cx;
+				y = 2 * x * y + cy;
+				x = x_tmp;
+				iter++;
+			}
+			double value = fabs((iter / (float)MAX_ITER)) * 255;
+			image[i][j] = value;
+		}
+	}
 }
 
 int main() {
