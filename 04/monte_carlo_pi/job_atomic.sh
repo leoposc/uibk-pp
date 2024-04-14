cbd41a8a12b87074a631c2b590e40334cfc22d1a
philippgothert <Philipp.Goethert@student.uibk.ac.at>
2024-04-14 16:33:14 +0200

    created job files

---
+++ 04/monte_carlo_pi/job_atomic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define the number of threads to benchmark
+threads=(1 2 4 6 12)
+
+# Loop through each number of threads and run the benchmark
+for t in "${threads[@]}"
+do
+    echo "Benchmarking mc_pi_atomic with $t threads..."
+    export OMP_NUM_THREADS=$t
+    time ./mc_pi_atomic
+    echo "----------------------------------------------"
+done
