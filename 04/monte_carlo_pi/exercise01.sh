58794798fd639ca14d578262e219f87cc2311a4f
LV Informatik <cb761028@login.lcc3.uibk.ac.at>
2024-04-15 11:45:29 +0200

    add execution time measurements for exercise 01

--- 04/monte_carlo_pi/exercise01.sh
+++ 04/monte_carlo_pi/exercise01.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+# Execute job in the partition "lva" unless you have special requirements.
+#SBATCH --partition=lva
+# Name your job to be able to identify it later
+#SBATCH --job-name test
+# Redirect output stream to this file
+#SBATCH --output=output2.log
+# Maximum number of tasks (=processes) to start in total
+#SBATCH --ntasks=1
+# Maximum number of tasks (=processes) to start per node
+#SBATCH --ntasks-per-node=1
+# Enforce exclusive node allocation, do not share with other jobs
+#SBATCH --exclusive
+
+
 # Define the number of threads to benchmark
 threads=(1 2 4 6 12)
 programs=("mc_pi_atomic" "mc_pi_critical" "mc_pi_reduction" "mc_pi_pthreads")
-
+# programs=("mc_pi_critical" )
 # Loop through each number of threads and run the benchmark
 for p in "${programs[@]}"
 do
     for t in "${threads[@]}"
     do
         echo "Benchmarking $p with $t threads..."
-        time ./$p $t
+        ./$p $t
         echo "----------------------------------------------"
     done
 done
