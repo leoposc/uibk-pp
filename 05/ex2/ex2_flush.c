afdcfaa4fa7b6bf3bda69bd235bf6a8866be2c75
LV Informatik <cb761028@login.lcc3.uibk.ac.at>
2024-04-22 13:54:58 +0200

    remove redundant flushes

--- 05/ex2/ex2_flush.c
+++ 05/ex2/ex2_flush.c
@@ -2,30 +2,33 @@
 #include <stdio.h>
 
 int main() {
-  int data, flag = 0;
-#pragma omp parallel num_threads(2)
-  {
-    if (omp_get_thread_num() == 0) {
 
-      data = 42;
-      // Flush data to thread 1 and strictly order the write to data
-      relative to the write to the flag * /
-#pragma omp flush(flag, data)
+    int data;
+    int flag = 0;
+
+    #pragma omp parallel num_threads(2)
+    {
+      if (omp_get_thread_num() == 0) {
+
+          data = 42;
 
           flag = 1;
-      // Flush flag to ensure that thread 1 sees the change
-#pragma omp flush(flag)
 
-    } else if (omp_get_thread_num() == 1) {
-      // Flush to read flag correctly
-#pragma omp flush(flag, data)
-      while (flag < 1) {
-#pragma omp flush(flag, data)
+      } else if(omp_get_thread_num() == 1) {
+        
+          int flag_val = 0;
+
+          while (flag_val < 1) {
+    #pragma omp flush(flag)
+             flag_val = flag;
+
+          }
+
+          printf("flag=%d data=%d\n", flag, data);
+
+          printf("flag=%d data=%d\n", flag, data);
+
       }
-      // Flush, because access to both flag and data in print function
-#pragma omp flush(flag, data)
-      printf("flag=%d data=%d\n", flag, data);
     }
-  }
-  return 0;
+    return 0;
 }
