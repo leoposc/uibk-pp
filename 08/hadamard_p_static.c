cd0f29b85ef8f1974da62f5384cf17d2fdbade62
leoposc <cb761028@login.lcc3.uibk.ac.at>
2024-05-13 13:39:03 +0200

    add exercie 08

---
+++ 08/hadamard_p_static.c
@@ -0,0 +1,50 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+    
+    if (argc != 2) {
+        printf("Usage: %s <n>\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+
+    int **a = malloc(n * sizeof(int *));
+    int **b = malloc(n * sizeof(int *));
+    int **c = malloc(n * sizeof(int *));
+
+    #pragma omp parallel for schedule(static) 
+    for (int i = 0; i < n; ++i) {
+        a[i] = malloc(n * sizeof(int));
+        b[i] = malloc(n * sizeof(int));
+        c[i] = malloc(n * sizeof(int));
+    }
+
+    #pragma omp parallel for schedule(static) 
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            a[i][j] = i + j;
+            b[i][j] = i - j;
+        }
+    }
+
+     // measure time
+    double start = omp_get_wtime();
+
+    #pragma omp parallel for schedule(static) 
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            c[i][j] = a[i][j] * b[i][j];
+        }
+    }
+
+    // calculate sum
+    double end = omp_get_wtime();
+    
+    printf("$!%s_%d_%d: %f\n",argv[0], n, omp_get_max_threads(), end - start);
+
+    return 0;
+}
---
+++ 08/job03.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Execute job in the partition "lva" unless you have special requirements.
+#SBATCH --partition=lva
+# Name your job to be able to identify it later
+#SBATCH --job-name exercise08
+# Redirect output stream to this file
+#SBATCH --output=ex02.log
+# Maximum number of tasks (=processes) to start in total
+#SBATCH --ntasks=12
+# Maximum number of tasks (=processes) to start per node
+#SBATCH --ntasks-per-node=12
+# Enforce exclusive node allocation, do not share with other jobs
+#SBATCH --exclusive
+
+# Define the number of threads to benchmark
+ts=(1 2 4 6 12)
+# ts=(12)
+# ns=(6)
+programs=("hadamard_p_static" "hadamard_serial" "hadamard_p_dynamic" "hadamard_p_guided" "hadamard_p_auto" "hadamard_p_runtime")
+# define the problem size:
+ns=(10000 20000 30000)
+# ns=(65536)
+# ns=(2000000)
+
+# Loop through each number of threads and run the benchmark
+for p in "${programs[@]}"
+do
+    for t in "${ts[@]}"
+    do
+        for n in "${ns[@]}"
+        do
+            echo "Benchmarking $p (problem size: $n) with $t threads..."
+            export OMP_NUM_THREADS=$t
+            ./$p $n
+            echo "----------------------------------------------"
+            echo ;
+        done
+    done
+done
